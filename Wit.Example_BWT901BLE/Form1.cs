e30aa27c4bec3b8948cd7ca28ba38ae0228c617c
Ouyang-Jiahong <ouyangjiahong22@outlook.com>
2024-08-24 21:58:01 +0800

    删去了英文注释及无用注释

--- Wit.Example_BWT901BLE/Form1.cs
+++ Wit.Example_BWT901BLE/Form1.cs
@@ -26,25 +26,21 @@ namespace Wit.Example_BWT901BLE
 
         /// <summary>
         /// 蓝牙管理器
-        /// Bluetooth manager
         /// </summary>
         private IWinBlueManager WitBluetoothManager = WinBlueFactory.GetInstance();
 
         /// <summary>
         /// 找到的设备
-        /// Found device
         /// </summary>
         private Dictionary<string, Bwt901ble> FoundDeviceDict = new Dictionary<string, Bwt901ble>();
 
         /// <summary>
         /// 控制自动刷新数据线程是否工作
-        /// Control whether the automatic refresh data thread works
         /// </summary>
         public bool EnableRefreshDataTh { get; private set; }
 
         /// <summary>
-        /// 构造
-        /// Structure
+        /// 构造器
         /// </summary>
         public Form1()
         {
@@ -53,7 +49,6 @@ namespace Wit.Example_BWT901BLE
 
         /// <summary>
         /// 窗体加载时
-        /// When the form is loaded
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -72,35 +67,29 @@ namespace Wit.Example_BWT901BLE
 
         /// <summary>
         /// 窗体关闭时
-        /// When the form is closed
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             // 关闭刷新数据线程
-            // Close refresh data thread
             EnableRefreshDataTh = false;
             // 关闭蓝牙搜索
-            // Turn off Bluetooth search
             stopScanButton_Click(null, null);
             Process.GetCurrentProcess().Kill();
         }
 
         /// <summary>
         /// 开始搜索
-        /// Starting the Search
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void startScanButton_Click(object sender, EventArgs e)
         {
             // 清除找到的设备
-            // Clear found devices
             FoundDeviceDict.Clear();
 
             // 关闭之前打开的设备
-            // Close previously opened devices
             for (int i = 0; i < FoundDeviceDict.Count; i++)
             {
                 var keyValue = FoundDeviceDict.ElementAt(i);
@@ -114,14 +103,12 @@ namespace Wit.Example_BWT901BLE
 
         /// <summary>
         /// 当搜索到蓝牙设备时会回调这个方法
-        /// Call back this method when Bluetooth devices are found
         /// </summary>
         /// <param name="mac"></param>
         /// <param name="deviceName"></param>
         private void WitBluetoothManager_OnDeviceFound(string mac, string deviceName)
         {
             // 名称过滤
-            // Name filtering
             if (deviceName != null && deviceName.Contains("WT"))
             {
                 if (!FoundDeviceDict.ContainsKey(mac))
@@ -129,7 +116,6 @@ namespace Wit.Example_BWT901BLE
                     Bwt901ble bWT901BLE = new Bwt901ble(mac,deviceName);
                     FoundDeviceDict.Add(mac, bWT901BLE);
                     // 打开这个设备
-                    // Open this device
                     bWT901BLE.Open();
                     bWT901BLE.OnRecord += BWT901BLE_OnRecord;
                 }
@@ -138,7 +124,6 @@ namespace Wit.Example_BWT901BLE
 
         /// <summary>
         /// 当传感器数据刷新时会调用这里，您可以在这里记录数据
-        /// This will be called when the sensor data is refreshed, where you can record the data
         /// </summary>
         /// <param name="BWT901BLE"></param>
         private void BWT901BLE_OnRecord(Bwt901ble BWT901BLE)
@@ -149,20 +134,17 @@ namespace Wit.Example_BWT901BLE
 
         /// <summary>
         /// 停止搜索
-        /// stop searching
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void stopScanButton_Click(object sender, EventArgs e)
         {
             // 让蓝牙管理器停止搜索
-            // Stop Bluetooth Manager Search
             WitBluetoothManager.StopScan();
         }
 
         /// <summary>
         /// 设备状态发生时会调这个方法
-        /// This method will be called when the device status occurs
         /// </summary>
         /// <param name="macAddr"></param>
         /// <param name="mType"></param>
@@ -172,21 +154,18 @@ namespace Wit.Example_BWT901BLE
             if (mType == 20)
             {
                 // 断开连接
-                // Disconnect
                 Debug.WriteLine(macAddr + "Disconnect");
             }
 
             if (mType == 11)
             {
                 // 连接失败
-                // Connect failed
                 Debug.WriteLine(macAddr + "Connect failed");
             }
 
             if (mType == 10)
             {
                 // 连接成功
-                // Successfully connected
                 Debug.WriteLine(macAddr + "Successfully connected");
             }
         }
@@ -200,11 +179,9 @@ namespace Wit.Example_BWT901BLE
             while (EnableRefreshDataTh)
             {
                 // 多设备的展示数据
-                // Display data for multiple devices
                 string DeviceData = "";
                 Thread.Sleep(100);
                 // 刷新所有连接设备的数据
-                // Refresh data for all connected devices
                 for (int i = 0; i < FoundDeviceDict.Count; i++)
                 {
                     var keyValue = FoundDeviceDict.ElementAt(i);
@@ -223,7 +200,6 @@ namespace Wit.Example_BWT901BLE
 
         /// <summary>
         /// 获得设备的数据
-        /// Obtaining device data
         /// </summary>
         private string GetDeviceData(Bwt901ble BWT901BLE)
         {
@@ -257,14 +233,12 @@ namespace Wit.Example_BWT901BLE
 
         /// <summary>
         /// 加计校准
-        /// Acceleration calibration
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void appliedCalibrationButton_Click(object sender, EventArgs e)
         {
             // 所有连接的蓝牙设备都加计校准
-            // All connected Bluetooth devices are calibrated
             for (int i = 0; i < FoundDeviceDict.Count; i++)
             {
                 var keyValue = FoundDeviceDict.ElementAt(i);
@@ -278,14 +252,8 @@ namespace Wit.Example_BWT901BLE
                 try
                 {
                     // 解锁寄存器并发送命令
-                    // Unlock register and send command
                     bWT901BLE.UnlockReg();
                     bWT901BLE.AppliedCalibration();
-
-                    // 下面两行与上面等价,推荐使用上面的
-                    // The following two lines are equivalent to the above, and it is recommended to use the above one
-                    //bWT901BLE.SendProtocolData(new byte[] { 0xff, 0xaa, 0x69, 0x88, 0xb5 });
-                    //bWT901BLE.SendProtocolData(new byte[] { 0xff, 0xaa, 0x01, 0x01, 0x00 });
                 }
                 catch (Exception ex)
                 {
@@ -296,7 +264,6 @@ namespace Wit.Example_BWT901BLE
 
         /// <summary>
         /// 设置回传速率200Hz
-        /// Set the return rate to 200Hz
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -314,14 +281,8 @@ namespace Wit.Example_BWT901BLE
                 try
                 {
                     // 解锁寄存器并发送命令
-                    // Unlock register and send command
                     bWT901BLE.UnlockReg();
                     bWT901BLE.SetReturnRate(0x0A);
-
-                    // 下面两行与上面等价,推荐使用上面的
-                    // The following two lines are equivalent to the above, and it is recommended to use the above one
-                    //bWT901BLE.SendProtocolData(new byte[] { 0xff, 0xaa, 0x69, 0x88, 0xb5 });
-                    //bWT901BLE.SendProtocolData(new byte[] { 0xff, 0xaa, 0x03, 0x08, 0x00 });
                 }
                 catch (Exception ex)
                 {
@@ -361,14 +322,12 @@ namespace Wit.Example_BWT901BLE
 
         /// <summary>
         /// 开始磁场校准
-        /// Start magnetic field calibration
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void startFieldCalibrationButton_Click(object sender, EventArgs e)
         {
             // 开始所有连接的蓝牙设备的磁场校准
-            // Start magnetic field calibration for all connected Bluetooth devices
             for (int i = 0; i < FoundDeviceDict.Count; i++)
             {
                 var keyValue = FoundDeviceDict.ElementAt(i);
@@ -381,14 +340,8 @@ namespace Wit.Example_BWT901BLE
                 try
                 {
                     // 解锁寄存器并发送命令
-                    // Unlock register and send command
                     bWT901BLE.UnlockReg();
                     bWT901BLE.StartFieldCalibration();
-
-                    // 下面两行与上面等价,推荐使用上面的
-                    // The following two lines are equivalent to the above, and it is recommended to use the above one
-                    //bWT901BLE.SendProtocolData(new byte[] { 0xff, 0xaa, 0x69, 0x88, 0xb5 });
-                    //bWT901BLE.SendProtocolData(new byte[] { 0xff, 0xaa, 0x01, 0x07, 0x00 });
                     MessageBox.Show("开始磁场校准,请绕传感器XYZ三轴各转一圈,转完以后点击【结束磁场校准】");
                 }
                 catch (Exception ex)
@@ -400,7 +353,6 @@ namespace Wit.Example_BWT901BLE
 
         /// <summary>
         /// 结束磁场校准
-        /// End magnetic field calibration
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -408,7 +360,6 @@ namespace Wit.Example_BWT901BLE
         {
 
             // 结束所有连接的蓝牙设备的磁场校准
-            // End the magnetic field calibration of all connected Bluetooth devices
             for (int i = 0; i < FoundDeviceDict.Count; i++)
             {
                 var keyValue = FoundDeviceDict.ElementAt(i);
@@ -421,14 +372,8 @@ namespace Wit.Example_BWT901BLE
                 try
                 {
                     // 解锁寄存器并发送命令
-                    // Unlock register and send command
                     bWT901BLE.UnlockReg();
                     bWT901BLE.EndFieldCalibration();
-
-                    // 下面两行与上面等价,推荐使用上面的
-                    // The following two lines are equivalent to the above, and it is recommended to use the above one
-                    //bWT901BLE.SendProtocolData(new byte[] { 0xff, 0xaa, 0x69, 0x88, 0xb5 });
-                    //bWT901BLE.SendProtocolData(new byte[] { 0xff, 0xaa, 0x01, 0x00, 0x00 });
                 }
                 catch (Exception ex)
                 {
@@ -438,7 +383,7 @@ namespace Wit.Example_BWT901BLE
         }
 
         /// <summary>
-        /// 时间处理器
+        /// 事件处理器
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
